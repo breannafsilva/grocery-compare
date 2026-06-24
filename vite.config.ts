6aad9d1e4f8968407f849a6f2d572af11374e332
BSILVA <breanna.silva@vizienitnc.com>
2026-06-24 13:28:57 -0500

    Use Kroger certification location

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ const { d1, r2 } = hostingConfig;
 const runtimeVars = {
   KROGER_AUTH_BASE_URL: "https://api-ce.kroger.com/v1/connect/oauth2/token",
   KROGER_API_BASE_URL: "https://api-ce.kroger.com/v1/products",
-  KROGER_LOCATION_ID: "53100516",
+  KROGER_LOCATION_ID: "53100508",
   KROGER_SCOPE: "product.compact",
   NEXT_PUBLIC_DEFAULT_POSTAL_CODE: "60647",
 };
